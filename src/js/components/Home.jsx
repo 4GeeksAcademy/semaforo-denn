7929be22a514e8c0d49874775006eda18efcab15
denn-p <dennielysp@gmail.com>
2025-12-17 12:24:02 +0000

    First commit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import "./HomeStyles.css";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [lightActive, setLightActive] = useState ("  ")
+
+	return (
+		<div className="traffic-light"> 
+
+			<div className="d-flex justify-content-center p-4">
+				<div className="bg-dark p-3 rounded-4 d-flex flex-column gap-3 ">
+
+					<button type="button" id="red-light" className={`rounded-circle bg-danger  ${lightActive == "red" ? "lightActiveGlow" : " " } `} onClick= {() => setLightActive("red")} ></button>
+
+					<button type="button" id="yellow-light" className={`rounded-circle bg-warning ${lightActive == "yellow" ? "lightActiveGlow" : " " }`} onClick= {() => setLightActive("yellow")} ></button>
+
+					<button type="button" id="green-light" className={`rounded-circle bg-success ${lightActive == "green" ? "lightActiveGlow" : " " }`} onClick= {() => setLightActive("green")} ></button>
+
+
+				</div>
+			</div>
+		
 		</div>
+
 	);
 };
 
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import "./components/HomeStyles.css";
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
